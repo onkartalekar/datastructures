d020cb40694ef0a889f6f077da0805bd94321134
Onkar Talekar <onkar.talekar@compass-usa.com>
2019-02-01 16:44:49 -0500

    LRU cache implementation

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,6 +1,16 @@
+import org.apache.commons.lang3.time.DateUtils;
+
+import java.util.Calendar;
+import java.util.Date;
+
 public class Solution {
-	
-	public static void main(String[] args) {
-	
-	}
+
+    public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date);
+        System.out.println(DateUtils.truncate(date, Calendar.DAY_OF_MONTH));
+        System.out.println(DateUtils.getFragmentInHours(date, Calendar.DAY_OF_MONTH));
+        System.out.println(DateUtils.getFragmentInMinutes(date, Calendar.HOUR_OF_DAY));
+        System.out.println(DateUtils.getFragmentInSeconds(date, Calendar.MINUTE));
+    }
 }
