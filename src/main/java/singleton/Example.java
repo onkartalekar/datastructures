3196c5b8280e4448b05c631d0d534fb467a41ee8
Onkar Talekar <onkar.talekar@gmail.com>
2019-02-02 01:21:41 -0500

    Singleton update

--- src/main/java/singleton/Example.java
+++ src/main/java/singleton/Example.java
@@ -5,23 +5,23 @@ package singleton;
  */
 
 public class Example {
-	
-	private static Example instance;
-	
-	private Example() {
-	}
-	
-	public static Example getInstance() {
-		if (instance != null) {
-			return instance;
-		}
-		synchronized (Example.class) {
-			if (instance == null) {
-				instance = new Example();
-				return instance;
-			}
-		}
-		return instance;
-	}
-	
+
+    private static Example instance;
+
+    private Example() {
+    }
+
+    public static Example getInstance() {
+        if (instance != null) {
+            return instance;
+        }
+
+        synchronized (Example.class) {
+            if (instance == null) {
+                instance = new Example();
+            }
+            return instance;
+        }
+    }
+
 }
